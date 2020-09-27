bdfbc5a7728f4f2b9cc93cc4947b8e1c7e27d764
Rishikesh <rishiyadav280198@gmail.com>
2020-09-27 19:07:30 +0530

    CRUD ops

---
+++ api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from api import models
+
+class InstituteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Institute
+        fields = "__all__"
+
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    institute = InstituteSerializer(read_only = True)#nested serializer
+    class Meta:
+        model = models.Student
+        fields = "__all__"
+
---
+++ api/views.py
@@ -0,0 +1,24 @@
+from rest_framework.response import Response
+from api import models,serializers
+from rest_framework.generics import(
+ ListAPIView,
+ RetrieveAPIView,
+ UpdateAPIView,
+ RetrieveUpdateAPIView,
+ ListCreateAPIView,
+ DestroyAPIView,
+ RetrieveUpdateDestroyAPIView
+)
+
+# Create your views here.
+class StudentListView(ListCreateAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = serializers.StudentSerializer
+
+class StudentDetailsView(RetrieveUpdateAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = serializers.StudentSerializer
+
+class StudentDeleteView(DestroyAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class  = serializers.StudentSerializer
